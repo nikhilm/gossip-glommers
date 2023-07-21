7227aac9dc73426332dfe252dde4dbddfbb7a91a
Nikhil Marathe <me@nikhilism.com>
2023-07-20 22:25:01 -0700

    Add basic broadcast solution

---
+++ 03_broadcast/3a.rkt
@@ -0,0 +1,36 @@
+#lang racket
+(require maelstrom)
+
+(define storage null)
+(define storage-sema (make-semaphore 1))
+(define node (make-std-node))
+
+(add-handler
+ node
+ "broadcast"
+ (lambda (req)
+   (call-with-semaphore storage-sema
+                        (lambda ()
+                          (set! storage (cons (message-ref req 'message) storage))))
+   (respond (make-response req))))
+
+(add-handler
+ node
+ "read"
+ (lambda (req)
+   (respond
+    (make-response req
+                   `(messages . ,(call-with-semaphore
+                                                    storage-sema
+                                                    ; make a copy of the list.
+                                                    (lambda () (map values storage))))))))
+
+(add-handler
+ node
+ "topology"
+ (lambda (req)
+   ; Nothing to be done for now.
+   (respond (make-response req))))
+
+(module+ main
+  (run node))

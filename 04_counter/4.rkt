691afee7accfab5a2cabfcd73d729b17f5fd37c3
Nikhil Marathe <me@nikhilism.com>
2023-09-13 16:41:05 -0700

    solution to challenge 4

--- 04_counter/4.rkt
+++ 04_counter/4.rkt
@@ -16,7 +16,7 @@
 (define (kv-read k [default 0])
   (define resp (kv-operation (make-message (hash 'type "read"
                                                  'key k))))
-  (log-counter-debug "read ~v result ~v" k resp)
+  #;(log-counter-debug "read ~v result ~v" k resp)
   (case (message-type resp)
     [("error") default]
     [("read_ok") (message-ref resp 'value)]
@@ -28,7 +28,7 @@
                                                  'value v))))
   (when (not (equal? "write_ok" (message-type resp)))
     (error 'kv-write "Unexpected response ~v" resp))
-  (log-counter-debug "write ~v ~v ok" k v))
+  #;(log-counter-debug "write ~v ~v ok" k v))
 
 (define (kv-cas k from to #:create-if-missing? [create-if-missing #f])
   (define resp (kv-operation (make-message (hash 'type "cas"
@@ -40,7 +40,7 @@
     [("cas_ok") #t]
     [("error") (case (message-ref resp 'code)
                  [(22) (begin
-                         (log-counter-debug "Failing cas ~v ~v ~v because: ~v" k from to (message-ref resp 'text))
+                         (log-counter-debug "CAS did not succeed ~v ~v ~v because: ~v" k from to (message-ref resp 'text))
                          #f)]
                  [(20) (error 'kv-cas "~v: ~v" k (message-ref resp 'text))])]
     [else (error 'kv-cas "Unexpected response type ~v" (message-type resp))]))
@@ -53,14 +53,20 @@
 (add-handler node
              "add"
              (lambda (req)
-               (define current (kv-read (node-key)))
-               (kv-write (node-key) (+ current (message-ref req 'delta)))
+               ; Since each node only updates its own entry, the CAS is to prevent
+               ; inconsistencies if the different handler threads ran out of order.
+               (let loop ()
+                 (define current (kv-read (node-key)))
+                 (define new (+ current (message-ref req 'delta)))
+                 (unless (kv-cas (node-key) current new #:create-if-missing? #t)
+                   (loop)))
                (respond req)))
 
 (add-handler node
              "read"
              (lambda (req)
-               (respond req (hash 'value (kv-read (node-key))))))
+               (define all-keys (cons (node-key) (known-peers)))
+               (respond req (hash 'value (apply + (map kv-read all-keys))))))
 
 (module+ main
   (run node))
